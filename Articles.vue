23dde2c45cd545c33ac8540d3881a8c16a8fee24
Gha8 <147407283+Gha8@users.noreply.github.com>
2025-04-02 13:40:44 +0200

    Add files via upload

---
+++ Articles.vue
@@ -0,0 +1,34 @@
+<template>
+     <div>
+     <h2>Liste des articles</h2>
+ <ul>
+     <li v-for="article in articles" :key="article.id">
+     <h3>{{ article.name }}</h3>
+     <p>{{ article.description }}</p>
+     <p>{{ article.price }}</p>
+     </li>
+     </ul>
+     </div>
+    </template>
+    <script>
+ import axios from 'axios';
+    
+     export default {
+     data() {
+     return {
+ articles: []
+     };
+     },
+     created() {
+     axios.get('http://localhost:3000/articles')
+     .then(response => {
+     this.articles = response.data;
+     })
+     .catch(error => {
+     console.error('Erreur lors de la récupération des articles:', error);
+     });
+     }
+     };
+     </script>
+    
+    
